40cb2a2e18b4bc365ded147df4cd90d024e7fbf0
redhotk1m <kim@adsl.no>
2018-09-07 19:36:00 +0200

    Oppgave 1.1.

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -1,5 +1,18 @@
 public class Oblig1 {
 
+    public static int maks (int[] a){
+        int maksverdi;
+        for (int i = 0; i<a.length-1; i++){
+            int temp;
+            if (a[i]>a[i+1]){
+                temp = a[i];
+                a[i]=a[i+1];
+                a[i+1]=temp;
+            }
 
+        }
+        maksverdi = a[a.length-1];
+        return maksverdi;
+    }
 
 }
